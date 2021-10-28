a14aa9d6e5fbaad90a6d0f10bd2b3e78e3943dea
yatinagg <yatinagg12@gmail.com>
2021-10-28 21:15:44 +0530

    Panagram Check

--- Panagram.kt
+++ Panagram.kt
@@ -5,9 +5,11 @@ import kotlin.collections.HashMap
  * main function
  */
 fun main() {
+
     val str1 = Scanner(System.`in`)
     val inpString = str1.nextLine()
-    if( isPanagram(inpString))
+
+    if (isPanagram(inpString))
         println("$inpString is a Panagram")
     else
         println("$inpString is not a Panagram")
@@ -19,14 +21,19 @@ fun main() {
  * @return Boolean is panagram
  */
 fun isPanagram(inpString: String): Boolean {
+
     val lowerSentence = inpString.lowercase()
-    val hashMap : HashMap<Char, Int> = HashMap()
+    val hashMap: HashMap<Char, Int> = HashMap()
+
     for (i in lowerSentence.indices) {
         hashMap[lowerSentence[i]] = 1
     }
-    for( i in 'a'..'z'){
-        if (!hashMap.containsKey(i))
+
+    for (i in 'a'..'z') {
+        if (!hashMap.containsKey(i)) {
             return false
+        }
     }
+
     return true
 }
